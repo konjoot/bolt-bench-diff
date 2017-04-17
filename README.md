c7cb98ecd61ab040334f5829d982f27703e9d764
Sergey Maksimov <konjoot@gmail.com>
2017-04-17 20:41:00 +0300

    BoltDB benches.

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# bolt-bench-diff
-Diff between native bolt DB performance and performance with simple encryption layer.
+```
+➔ cat native.out
+Benchmark_CreatePerTx-4                  	     500	   3006573 ns/op
+Benchmark_PutDataByBatch-4               	  200000	    331578 ns/op
+Benchmark_PutDataConcurrentlyByBatch-4   	  200000	    327767 ns/op
+--- BENCH: Benchmark_PutDataConcurrentlyByBatch-4
+	bench_test.go:186: count keys = 5
+	bench_test.go:186: count keys = 104
+	bench_test.go:186: count keys = 10004
+	bench_test.go:186: count keys = 200004
+Benchmark_GetData-4                      	 1000000	      1021 ns/op
+Benchmark_Cursor-4                       	20000000	        54.5 ns/op
+PASS
+ok  	command-line-arguments	231.569s
+
+
+➔ cat encrypt.out
+Benchmark_CreatePerTx-4                  	     500	   3344007 ns/op
+Benchmark_PutDataByBatch-4               	  100000	    160170 ns/op
+Benchmark_PutDataConcurrentlyByBatch-4   	  100000	    160146 ns/op
+--- BENCH: Benchmark_PutDataConcurrentlyByBatch-4
+	bolt_encrypt_bench_test.go:192: count keys = 5
+	bolt_encrypt_bench_test.go:192: count keys = 104
+	bolt_encrypt_bench_test.go:192: count keys = 10004
+	bolt_encrypt_bench_test.go:192: count keys = 100004
+Benchmark_GetData-4                      	   50000	     32058 ns/op
+Benchmark_Cursor-4                       	 2000000	       918 ns/op
+PASS
+ok  	command-line-arguments	62.841s
+
+➔ benchcmp native.out encrypt.out
+benchmark                                  old ns/op     new ns/op     delta
+Benchmark_CreatePerTx-4                    3006573       3344007       +11.22%
+Benchmark_PutDataByBatch-4                 331578        160170        -51.69%
+Benchmark_PutDataConcurrentlyByBatch-4     327767        160146        -51.14%
+Benchmark_GetData-4                        1021          32058         +3039.86%
+Benchmark_Cursor-4                         54.5          918           +1584.40%
+
+```
---
+++ encryption/bench_test.go
@@ -0,0 +1,356 @@
+package encryption
+
+import (
+	"crypto/rand"
+	"fmt"
+	"os"
+	"sync"
+	"testing"
+
+	"github.com/reinventer/bolt"
+)
+
+func put(db *bolt.DB, key, val []byte) error {
+	return db.Update(func(tx *bolt.Tx) error {
+		// Retrieve the users bucket.
+		// This should be created when the DB is first opened.
+		b := tx.Bucket([]byte("MyBuket"))
+
+		// Persist bytes to users bucket.
+		return b.Put(key, val)
+	})
+}
+
+func Benchmark_CreatePerTx(b *testing.B) {
+	_ = os.Remove("test-data.db")
+	db, err := bolt.Open("test-data.db", 0600, &bolt.Options{
+		EncryptionKey: []byte(`keykeykeykeykey!`),
+	})
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer db.Close()
+
+	tx, err := db.Begin(true)
+	if err != nil {
+		b.Fatal(err)
+	}
+	_, err = tx.CreateBucket([]byte("MyBuket"))
+	if err != nil {
+		b.Error(err)
+	}
+	// Commit the transaction and check for error.
+	if err := tx.Commit(); err != nil {
+		b.Error(err)
+		tx.Rollback()
+	}
+	data := []byte(`aloxa aloxa aloxa aloxa aloxa aloxa aloxa aloxa aloxa aloxa aloxa aloxaaloxa aloxaaloxa aloxa aloxa aloxa aloxa aloxa aloxa aloxa`)
+	b.ResetTimer()
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			if err = put(db, generateID(), data); err != nil {
+				b.Error(err)
+			}
+		}
+	})
+}
+
+func generateID() []byte {
+	b := make([]byte, 8)
+	_, _ = rand.Read(b)
+	return b
+}
+
+func Benchmark_PutDataByBatch(b *testing.B) {
+	_ = os.Remove("test-data.db")
+	db, err := bolt.Open("test-data.db", 0600, &bolt.Options{
+		EncryptionKey: []byte(`keykeykeykeykey!`),
+	})
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer db.Close()
+
+	tx, err := db.Begin(true)
+	if err != nil {
+		b.Fatal(err)
+	}
+	_, err = tx.CreateBucket([]byte("MyBuket"))
+	if err != nil {
+		b.Error(err)
+	}
+	// Commit the transaction and check for error.
+	if err := tx.Commit(); err != nil {
+		b.Error(err)
+		tx.Rollback()
+	}
+
+	data := []byte(`aloxa aloxa aloxa aloxa aloxa aloxa aloxa aloxa aloxa aloxa aloxa aloxaaloxa aloxaaloxa aloxa aloxa aloxa aloxa aloxa aloxa aloxa`)
+
+	txw, err := db.Begin(true)
+	if err != nil {
+		b.Error(err)
+	}
+	var errW error
+
+	b.ResetTimer()
+
+	// concurrecy bdon worket by default
+	//b.RunParallel(func(pb *testing.PB) {
+	//	for pb.Next() {
+	//		if errW = txw.Bucket([]byte(`MyBuket`)).Put(generateID(), data); errW != nil {
+	//			b.Error(errW)
+	//		}
+	//	}
+	//})
+
+	bucket := txw.Bucket([]byte(`MyBuket`))
+	for i := 0; i < b.N; i++ {
+		if errW = bucket.Put(generateID(), data); errW != nil {
+			b.Error(errW)
+		}
+	}
+
+	if errW != nil {
+		txw.Rollback()
+	} else {
+		txw.Commit()
+	}
+}
+
+func Benchmark_PutDataConcurrentlyByBatch(b *testing.B) {
+	_ = os.Remove("test-data.db")
+	db, err := bolt.Open("test-data.db", 0600, &bolt.Options{
+		EncryptionKey: []byte(`keykeykeykeykey!`),
+	})
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer db.Close()
+
+	tx, err := db.Begin(true)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	_, err = tx.CreateBucket([]byte("MyBuket"))
+	if err != nil {
+		b.Error(err)
+	}
+	// Commit the transaction and check for error.
+	if err := tx.Commit(); err != nil {
+		b.Error(err)
+		tx.Rollback()
+	}
+
+	data := []byte(`aloxa aloxa aloxa aloxa aloxa aloxa aloxa aloxa aloxa aloxa aloxa aloxaaloxa aloxaaloxa aloxa aloxa aloxa aloxa aloxa aloxa aloxa`)
+
+	txw, err := db.Begin(true)
+	if err != nil {
+		b.Error(err)
+	}
+
+	putCh := make(chan []byte)
+	done := make(chan struct{})
+	go func() {
+		var errW error
+		bucket := txw.Bucket([]byte(`MyBuket`))
+		for key := range putCh {
+			if errW := bucket.Put(key, data); errW != nil {
+				b.Error(errW)
+			}
+		}
+		if errW != nil {
+			txw.Rollback()
+		} else {
+			txw.Commit()
+		}
+		close(done)
+	}()
+
+	// test for now start go routine
+	putCh <- generateID()
+
+	putCh <- generateID()
+	putCh <- generateID()
+	putCh <- generateID()
+
+	b.ResetTimer()
+
+	// concurrecy don't worket by default
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			putCh <- generateID()
+		}
+	})
+
+	close(putCh)
+	<-done
+	db.View(func(tx *bolt.Tx) error {
+		buck := tx.Bucket([]byte("MyBuket"))
+		b.Logf("count keys = %d", buck.Stats().KeyN)
+		return nil
+	})
+}
+
+func putter(b *testing.B, db *bolt.DB) (chan []byte, chan struct{}) {
+	data := []byte(`aloxa aloxa aloxa aloxa aloxa aloxa aloxa aloxa aloxa aloxa aloxa aloxaaloxa aloxaaloxa aloxa aloxa aloxa aloxa aloxa aloxa aloxa`)
+
+	txw, err := db.Begin(true)
+	if err != nil {
+		b.Error(err)
+	}
+
+	putCh := make(chan []byte)
+	done := make(chan struct{})
+	go func() {
+		var errW error
+		bucket := txw.Bucket([]byte(`MyBuket`))
+		for key := range putCh {
+			if errW := bucket.Put(key, data); errW != nil {
+				b.Error(errW)
+			}
+		}
+		if errW != nil {
+			txw.Rollback()
+		} else {
+			txw.Commit()
+		}
+		close(done)
+	}()
+	return putCh, done
+}
+
+// просто получение по ключу
+// в итераторе еще быстрее
+func Benchmark_GetData(b *testing.B) {
+	_ = os.Remove("test-data-reader.db")
+	db, err := bolt.Open("test-data-reader.db", 0600, &bolt.Options{
+		EncryptionKey: []byte(`keykeykeykeykey!`),
+	})
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer db.Close()
+
+	tx, err := db.Begin(true)
+	if err != nil {
+		b.Fatal(err)
+	}
+	_, err = tx.CreateBucket([]byte("MyBuket"))
+	if err != nil {
+		b.Error(err)
+	}
+	// Commit the transaction and check for error.
+	if err := tx.Commit(); err != nil {
+		b.Error(err)
+		tx.Rollback()
+	}
+
+	if b.N > 10000 {
+		wg := sync.WaitGroup{}
+		for j := 0; j < b.N/10000; j++ {
+			wg.Add(1)
+			go func(iter int) {
+				defer wg.Done()
+				in, done := putter(b, db)
+				start := iter * 10000
+				for i := start; i < start+10000; i++ {
+					in <- []byte(fmt.Sprintf("test-data-key-%d", i))
+				}
+				close(in)
+				<-done
+			}(j)
+		}
+		wg.Wait()
+	} else {
+		in, done := putter(b, db)
+		for i := 0; i < b.N; i++ {
+			in <- []byte(fmt.Sprintf("test-data-key-%d", i))
+		}
+
+		close(in)
+		<-done
+	}
+
+	txr, err := db.Begin(false)
+	reader := txr.Bucket([]byte(`MyBuket`))
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		if dataFind := reader.Get([]byte(fmt.Sprintf("test-data-key-%d", i))); dataFind == nil {
+			b.Errorf(`get nil data`)
+		}
+	}
+
+	txr.Rollback()
+}
+
+func Benchmark_Cursor(b *testing.B) {
+	_ = os.Remove("test-data-reader.db")
+	db, err := bolt.Open("test-data-reader.db", 0600, &bolt.Options{
+		EncryptionKey: []byte(`keykeykeykeykey!`),
+	})
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer db.Close()
+
+	tx, err := db.Begin(true)
+	if err != nil {
+		b.Fatal(err)
+	}
+	_, err = tx.CreateBucket([]byte("MyBuket"))
+	if err != nil {
+		b.Error(err)
+	}
+	// Commit the transaction and check for error.
+	if err := tx.Commit(); err != nil {
+		b.Error(err)
+		tx.Rollback()
+	}
+
+	if b.N > 10000 {
+		wg := sync.WaitGroup{}
+		for j := 0; j < b.N/10000; j++ {
+			wg.Add(1)
+			go func(iter int) {
+				defer wg.Done()
+				in, done := putter(b, db)
+				start := iter * 10000
+				for i := start; i < start+10000; i++ {
+					in <- []byte(fmt.Sprintf("test-data-key-%d", i))
+				}
+				close(in)
+				<-done
+			}(j)
+		}
+		wg.Wait()
+	} else {
+		in, done := putter(b, db)
+		for i := 0; i < b.N; i++ {
+			in <- []byte(fmt.Sprintf("test-data-key-%d", i))
+		}
+
+		close(in)
+		<-done
+	}
+
+	txr, err := db.Begin(false)
+	reader := txr.Bucket([]byte(`MyBuket`))
+
+	curs := reader.Cursor()
+
+	b.ResetTimer()
+
+	curs.First()
+	for i := 0; i < b.N-1; i++ {
+		if _, dataFind := curs.Next(); dataFind == nil {
+			b.Errorf(`get nil data`)
+		}
+	}
+
+	txr.Rollback()
+}
